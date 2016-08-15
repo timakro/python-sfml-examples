eb518665e06302684f1f6e70dec1c151134dbf9b
Tim Schumacher <tim@timakro.de>
2016-08-15 15:03:14 +0200

    Add window.py

---
+++ window.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sfml
+import time
+
+window = sfml.graphics.RenderWindow(
+    sfml.window.VideoMode(640, 480),
+    title="My first pySFML window"
+)
+
+while window.is_open:
+    for event in window.events:
+        if isinstance(event, sfml.window.CloseEvent):
+            window.close()
+
+    window.clear(sfml.Color.RED)
+    window.display()
